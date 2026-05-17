a4bff44be69dd3eb9baff9a2848f2b7918761c99
priyakhatri723-netizen <priyakhatri723@gmail.com>
2026-05-17 10:49:31 +0530

    Create KeybindRowMixin.java

---
+++ src/main/java/com/chirag/bindyourkeys/mixin/KeybindRowMixin.java
@@ -0,0 +1,15 @@
+package com.chirag.bindyourkeys.mixin;
+
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(targets = "net.minecraft.class_4185$class_12231", remap = false)
+public abstract class KeybindRowMixin {
+
+    @Inject(method = "method_75752", at = @At("HEAD"), cancellable = true)
+    private void onRender(net.minecraft.class_332 graphics,
+            int mouseX, int mouseY, float delta, CallbackInfo ci) {
+        ci.cancel();
+    }
+}
