3ff2726ea3bf9f12deb255b486da7dba6962c723
priyakhatri723-netizen <priyakhatri723@gmail.com>
2026-05-17 10:58:44 +0530

    Update KeybindRowMixin.java

--- src/main/java/com/chirag/bindyourkeys/mixin/KeybindRowMixin.java
+++ src/main/java/com/chirag/bindyourkeys/mixin/KeybindRowMixin.java
@@ -1,6 +1,8 @@
 package com.chirag.bindyourkeys.mixin;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.screens.options.controls.KeyBindsScreen;
 import org.spongepowered.asm.mixin.*;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -11,6 +13,9 @@ public abstract class KeybindRowMixin {
     @Inject(method = "method_75752", at = @At("HEAD"), cancellable = true)
     private void onRender(GuiGraphics graphics,
             int mouseX, int mouseY, float delta, CallbackInfo ci) {
-        ci.cancel();
+        Minecraft mc = Minecraft.getInstance();
+        if (mc != null && mc.screen instanceof KeyBindsScreen) {
+            ci.cancel();
+        }
     }
 }
